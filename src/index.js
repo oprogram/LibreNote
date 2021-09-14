72fabdd2fb2e48b33d944f535c3a51dfd30642cd
Ulferno <36534987+Ulferno@users.noreply.github.com>
2021-09-14 19:53:21 +0100

    deploy stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-const test = 'd';
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-console.log(test);
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setPresence({ activities: [{ name: 'music' }], status: 'online' });
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
